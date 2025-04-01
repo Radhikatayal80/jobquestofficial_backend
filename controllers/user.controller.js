c9249eed53f64c393a2d9410c9a293b2578f8a51
Radhika Tayal <116450121+Radhikatayal80@users.noreply.github.com>
2025-04-01 15:54:22 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -95,6 +95,7 @@ export const login = async (req, res) => {
         return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, sameSite:"None",secure:true }).json({
             message: `Welcome back ${user.fullname}`,
             user,
+            token,
             success: true
         })
     } catch (error) {
